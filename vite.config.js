a7648a98f212289530d1d7dbd117c9d1722a0cf9
Nicksanford2323 <noreply@github.com>
2025-02-08 12:53:27 +0000

    Initial setup with deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/electrical-business-web-2025/',
+  base: '/electrical-business-web-1/',
 })
