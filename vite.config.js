b2000081a02b32d775cdd917ef8de1ae82b89eb5
nicksanford2323 <Sanfordn722@gmail.com>
2025-02-10 07:06:41 -0600

    Update vite.config.js in main

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/electrical-business-web-1/',
-})
+  base: command === 'serve' ? '/' : '/electrical-business-web-2/',
+  server: {
+    allowedHosts: [
+      '6b7a5e14-50b8-421e-908c-088f65437c4b-00-1d38azhgw8zpi.kirk.replit.dev'
+    ]
+    // Alternatively, if you have dynamic hosts on Replit, you can allow all hosts:
+    // allowedHosts: 'all'
+  }
+}))
