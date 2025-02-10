0d50acf885819786776ef56aab231fcd98ffec5e
nicksanford2323 <Sanfordn722@gmail.com>
2025-02-10 09:10:48 -0600

    Update vite.config.js in gh-pages-alt

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/electrical-business-web-1/',
+  base: '/electrical-business-web-1/', // The subfolder name of your GH Pages site
 })
