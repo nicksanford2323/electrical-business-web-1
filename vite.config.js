1f20c1c909537d4bf72cdc27318d2a932fa089fc
nicksanford2323 <Sanfordn722@gmail.com>
2025-02-10 07:55:30 -0600

    Update vite.config.js in main

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  // Use '/' as the base when running in development (i.e. on Replit)
-  // and '/electrical-business-web-2/' for production builds.
-  base: command === 'serve' ? '/' : '/electrical-business-web-2/',
-}))
+  base: '/electrical-business-web-1/',
+})
